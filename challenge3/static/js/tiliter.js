a2658994154326710ccdf065bb2de8e0c8e5dca3
Cameron Moore <c3166465@uon.edu.au>
2020-12-11 16:51:51 +1100

    Styling improved

---
+++ challenge3/static/js/tiliter.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function(){
+
+    // $.ajax({
+    //     url: '/get-movie-data',
+    //     type: 'POST',
+    //     success: function(result) {
+    //         console.log(result)
+    //         let movieData = result["movieData"]
+    //         let showTimes = result["showTimes"]
+    //         // result = JSON.parse(result)
+    //         // for (let i = 0; i < data.length; i++) {
+    //         //     if (data[i] == "movieData"){
+    //         //         let movieData = data[key]
+    //         //     }
+    //         //     else if (data[i] == "showTimes"){
+    //         //         let showTimes = data[key]
+    //         //     }
+    //         // }
+    //     }
+    // });
+
+
+});
--- challenge3/webapp.py
+++ challenge3/webapp.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template
+"""
+
+    This applicaiton is for coding challenge 3.1. It uses a python flask server to serve a simple search bar applcation
+"""
+
+
+from flask import Flask, render_template, jsonify
+import json
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    with open('data/movie_metadata.json') as jsonFile:
+        movieData = json.load(jsonFile)
+    #     movieData = {'movieData': movieData}
+    with open('data/theater_showtimes.json') as jsonFile:
+        showTimes = json.load(jsonFile)
+        # showTimes = {'showTimes': showTimes}
+    # movieData.update(showTimes)
+
+    return render_template('index.html', movieData=movieData, showTimes=showTimes)
+
+@app.route('/get-movie-data', methods = ['POST'])
+def getMovieData():
+    with open('data/movie_metadata.json') as jsonFile:
+        movieData = json.load(jsonFile)
+        movieData = {'movieData': movieData}
+    with open('data/theater_showtimes.json') as jsonFile:
+        showTimes = json.load(jsonFile)
+        showTimes = {'showTimes': showTimes}
+    movieData.update(showTimes)
+    return jsonify(movieData)
 
 app.run(debug=True,host='0.0.0.0')
