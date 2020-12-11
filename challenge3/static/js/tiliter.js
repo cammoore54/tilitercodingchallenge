b74b8016be04c2b52e3c53dd55b295e1e058300c
Cameron Moore <c3166465@uon.edu.au>
2020-12-11 22:24:38 +1100

    Search and theatre time working.

--- challenge3/static/js/tiliter.js
+++ challenge3/static/js/tiliter.js
@@ -1,39 +1,56 @@
 
 
+let theatreMovieList = [];
+let theatreMovieListDict = [];
+
+let movieListTotal = [];
+let movieListDictTotal = [];
+
+
 $(document).ready(function(){
 
-    let movieList = [];
-    let movieListDict = [];
-    let $elementList = $( ".media" ).each(function(idx, el){
+    $("#arclight-button").focus()
+    $("#arclight-button").click()
+    $("#arclight-button").click(function(){
+        getShowTimes('Arclight')
 
-        movieList.push($( this ).attr("name"))
-        movieListDict.push({
+    }); 
+    $("#pacific-theatres-button").click(function(){
+        getShowTimes('Pacific Theatres')
+
+    }); 
+    $("#amc-button").click(function(){
+        getShowTimes('AMC')
+
+    }); 
+
+    $( ".media" ).each(function(idx, el){
+
+        movieListTotal.push($( this ).attr("name"))
+        movieListDictTotal.push({
             name:  $( this ).attr("name"),
             id: $( this ).attr("id")
         });
-        //here this/el refers to the current image dom reference
-        //do soemthing
     })
-    console.log(movieList)
 
     $( "#movieSearch" ).keyup(function() {
         let input = $(this).val().toLowerCase()
 
-        let filteredList = movieList.filter(element => element.includes(input));
+        let filteredList = theatreMovieList.filter(element => element.includes(input));
         if (filteredList.length == 0){
-            for (let i=0; i < movieListDict.length; i++){
-                $("#"+ movieListDict[i]["id"]).hide()
+            for (let i=0; i < theatreMovieListDict.length; i++){
+                $("#"+ theatreMovieListDict[i]["id"]).hide()
             }
         }
         else{
-            for (let i=0; i < movieListDict.length; i++){
+            for (let i=0; i < theatreMovieListDict.length; i++){
                 for (let j=0; j < filteredList.length; j++){
-                    if (movieListDict[i]["name"]  == filteredList[j]){
-                        $("#"+ movieListDict[i]["id"]).show()
+                    if (theatreMovieListDict[i]["name"]  == filteredList[j]){
+                        $("#"+ theatreMovieListDict[i]["id"]).show()
                         break;
                     }
                     else{
-                        $("#"+ movieListDict[i]["id"]).hide()
+                        $("#"+ theatreMovieListDict[i]["id"]).hide()
                     }
                 }
             }
@@ -42,24 +59,63 @@ $(document).ready(function(){
         console.log(movieList.filter(element => element.includes(input) ) );
       });
 
-    // $.ajax({
-    //     url: '/get-movie-data',
-    //     type: 'POST',
-    //     success: function(result) {
-    //         console.log(result)
-    //         let movieData = result["movieData"]
-    //         let showTimes = result["showTimes"]
-    //         // result = JSON.parse(result)
-    //         // for (let i = 0; i < data.length; i++) {
-    //         //     if (data[i] == "movieData"){
-    //         //         let movieData = data[key]
-    //         //     }
-    //         //     else if (data[i] == "showTimes"){
-    //         //         let showTimes = data[key]
-    //         //     }
-    //         // }
-    //     }
-    // });
-
-
 });
+
+function getMovieList(showTimes){
+    let movieList = [];
+    let movieListDict = [];
+    for (let id in showTimes){
+        $( ".media" ).each(function(idx, el){
+            if ($( this ).attr("id") == id){
+                movieList.push($( this ).attr("name"))
+                movieListDict.push({
+                    name:  $( this ).attr("name"),
+                    id: $( this ).attr("id")
+                });
+            }
+        });
+    }
+    return [movieList, movieListDict]
+}
+
+
+function showHideMovies(filteredList){
+
+    for (let i=0; i < movieListDictTotal.length; i++){
+        for (let j=0; j < filteredList.length; j++){
+            if (movieListDictTotal[i]["name"]  == filteredList[j]){
+                $("#"+ movieListDictTotal[i]["id"]).show()
+                break;
+            }
+            else{
+                $("#"+ movieListDictTotal[i]["id"]).hide()
+            }
+        }
+    }
+}
+
+function getShowTimes(theatre){
+
+    $.ajax({
+        url: '/get-show-times/'+ theatre,
+        type: 'POST',
+        success: function(result) {
+            
+            let showTimes = result;
+            let movieLists = getMovieList(showTimes)
+            theatreMovieList = movieLists[0]
+            theatreMovieListDict = movieLists[1]
+            
+            showHideMovies(theatreMovieList)
+
+            for (let id in showTimes){
+                let timesString = "";
+                for (let i=0; i < showTimes[id].length; i++){
+                    timesString += " " + showTimes[id][i]
+                }
+                $("#"+id+"-times").text(timesString);
+            }
+        }
+    });
+      
+}
--- challenge3/webapp.py
+++ challenge3/webapp.py
@@ -20,15 +20,12 @@ def index():
 
     return render_template('index.html', movieData=movieData, showTimes=showTimes)
 
-@app.route('/get-movie-data', methods = ['POST'])
-def getMovieData():
-    with open('data/movie_metadata.json') as jsonFile:
-        movieData = json.load(jsonFile)
-        movieData = {'movieData': movieData}
+@app.route('/get-show-times/<theatre>', methods = ['POST'])
+def getShowTimes(theatre):
     with open('data/theater_showtimes.json') as jsonFile:
-        showTimes = json.load(jsonFile)
-        showTimes = {'showTimes': showTimes}
-    movieData.update(showTimes)
-    return jsonify(movieData)
+        showTimeList = json.load(jsonFile)
+    for theatreNames in showTimeList:
+        if theatreNames["name"] == theatre:
+            return jsonify(theatreNames["showtimes"])
 
 app.run(debug=True,host='0.0.0.0')
